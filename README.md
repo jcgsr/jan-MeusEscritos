3c784818157e52847725dac82f870a3580bd43a1
jcgsr <cabralguerra@gmail.com>
2022-01-12 09:02:12 -0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/7b24f660-a3a3-4900-ba9a-263e0d8bb74c/deploy-status)](https://app.netlify.com/sites/serene-franklin-379f80/deploys)
+
 # Meus Escritos
 
 Site criado para que os escritores possam divulgar suas obras sem custo algum.
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 	if (stage === "build-html") {
 		actions.setWebpackConfig({
@@ -13,31 +12,3 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 		});
 	}
 };
-=======
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions, getConfig }) => {
-//   if (stage === "build-html" || stage === "develop-html") {
-//     //   actions.setWebpackConfig({
-//     //     module: {
-//     //       rules: [
-//     //         {
-//     //           test: /reach-router/,
-//     //           use: loaders.null(),
-//     //         },
-//     //       ],
-//     //     },
-//     //   });
-//     externals: getConfig().externals.concat(function (
-//       context,
-//       request,
-//       callback
-//     ) {
-//       const regex = /^@?firebase(\/(.+))?/;
-//       // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-//       if (regex.test(request)) {
-//         return callback(null, "commonjs " + request); // <- use commonjs!
-//       }
-//       callback();
-//     });
-//   }
-// };
->>>>>>> 6770c565f710dd5d76827ca5d5d48c994e646ebe
